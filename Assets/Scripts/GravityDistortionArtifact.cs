f3e75efc2856e26fba3edee43fd3b54c534862f8
DESKTOP-6KR7LVK\Пользователь <lyana688@gmail.com>
2021-07-01 22:26:22 +0700

    Добавлены цифры в меню настройки артефакта

--- Assets/Scripts/GravityDistortionArtifact.cs
+++ Assets/Scripts/GravityDistortionArtifact.cs
@@ -26,7 +26,7 @@ public class GravityDistortionArtifact : MonoBehaviour
     public int directionOfGDA = 1;
 
     private PlayerControl player;
-    private Scrollbar scr;
+    private Slider scr;
 
     //1 - вдоль оси y, голова по оси
     //2 - вдоль оси y, голова против оси
@@ -106,8 +106,8 @@ public class GravityDistortionArtifact : MonoBehaviour
 
     public void setVale()
     {
-        scr = GameObject.Find("Scrollbar").GetComponent<Scrollbar>();
-        directionOfGDA = (int)Math.Round(scr.value*5+1); 
+        scr = GameObject.Find("Slider").GetComponent<Slider>();
+        directionOfGDA = (int)Math.Round(scr.value); 
     }
     private void activationGDA()
     {
